06082a301108d22f63076a5034bb80a93e4af1a9
CNSITM <psr@cnsitm.com>
2021-08-19 15:23:06 +0900

    mobile

--- App.js
+++ App.js
@@ -1,21 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import "react-native-gesture-handler";
+import React from "react";
+import { StyleSheet, SafeAreaView } from "react-native";
+import MainScreen from "./screens/main.js";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStacNavigator } from "@react-navigation/stack";
+
+const stack = createStacNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.safeAreaView}>
+      <MainScreen />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  safeAreaView: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#FFFFFF",
   },
 });
---
+++ config/constants.js
@@ -0,0 +1,1 @@
+export const API_URL = "https://8312709c7bf1.ngrok.io";
